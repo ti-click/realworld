1f9699e1b95412fc6d4a0a038e736b88051365a1
chengchen <cc8602@gmail.com>
2022-10-22 12:08:58 +0800

    1. update the database setting 2. add gitpod.yml 3. update the requirements

--- realworld/settings.py
+++ realworld/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = "realworld.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test', # Use the default database of TiDB
+        'USER': '${USERNAME}', # 2RZ3WzcNZTCDfFB.root
+        'PASSWORD': '${PASSWORD}', # qmSIgdoW2E09ATlO
+        'HOST': '${HOST}', # gateway01.us-west-2.prod.aws.tidbcloud.com
+        'PORT': '4000'
     }
 }
 
